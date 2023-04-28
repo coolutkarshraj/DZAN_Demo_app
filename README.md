98eef214cd8f7181313a99f2399e55577ca16ab0
Utkarsh Raj <utkarshcpr@gmail.com>
2023-04-28 14:39:26 +0530

    Count increment done

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ## DAZN Demo ExoPlayer App with Jetpack Compose
 
-This repository demonstrates how we can use ExoPlayer together with Jetpack Compose. You can find examples of how we can play video from remote and how we can play a radio url.
+This repository demonstrates how we can use ExoPlayer together with Jetpack Compose. 
+You can find examples of how we can play video from remote and how we can play a video url.
 - Compose Version : 1.0.5
 - ExoPlayer Version : 2.16.1
+- Dagger Hilt
+- Firebase Analytics
 
--- app/src/main/java/com/dzan/exoplayerdemo/MainActivity.kt
+++ app/src/main/java/com/dzan/exoplayerdemo/MainActivity.kt
@@ -56,46 +56,47 @@ fun Navigation(navController: NavHostController) {
 fun provideExoPlayer(context : Context, mediaItem: MediaItem): ExoPlayer {
     val player = ExoPlayer.Builder(context).build()
     player.setMediaItem(mediaItem)
+    var count = 0
     player.addListener(object : Player.Listener {
         override fun onPlaybackStateChanged(@Player.State state: Int) {
             val bundle: Bundle ? = null
             when (state) {
                 Player.STATE_READY -> {
-                    bundle?.putString(player.playbackState.toString(), player.currentTimeline.toString());
+                    bundle?.putString(player.playbackState.toString(), count++.toString());
                     fireBaseModule.logEvent(player.playbackState.toString(), bundle)
                     // The player is able to immediately play from its current position.
                 // The player will be playing if getPlayWhenReady() is true, and paused otherwise.
                 }
                 Player.STATE_BUFFERING -> {
-                    bundle?.putString(player.playbackState.toString(), player.currentTimeline.toString());
+                    bundle?.putString(player.playbackState.toString(), count++.toString());
                     fireBaseModule.logEvent(player.playbackState.toString(), bundle)
                     // The player is not able to immediately play the media,
                 // but is doing work toward being able to do so. This state typically occurs when the player needs to buffer more data before playback can start.
                 }
                 Player.STATE_IDLE -> {
-                    bundle?.putString(player.playbackState.toString(), player.currentTimeline.toString());
+                    bundle?.putString(player.playbackState.toString(), count++.toString());
                     fireBaseModule.logEvent(player.playbackState.toString(), bundle)
                     // The player is idle, meaning it holds only limited resources.
                 // The player must be prepared before it will play the media.
                 }
                 Player.STATE_ENDED -> {
-                    bundle?.putString(player.playbackState.toString(), player.currentTimeline.toString());
+                    bundle?.putString(player.playbackState.toString(), count++.toString());
                     fireBaseModule.logEvent(player.playbackState.toString(), bundle)
                     // The player has finished playing the media.
                 }
                 Player.COMMAND_PLAY_PAUSE ->{
                     // TCommand to start, pause or resume playback.
-                    bundle?.putString(player.playbackState.toString(), player.currentTimeline.toString())
+                    bundle?.putString(player.playbackState.toString(), count++.toString())
                     fireBaseModule.logEvent(player.playbackState.toString(), bundle)
                 }
                 Player.COMMAND_SEEK_FORWARD ->{
                     //Command to seek forward by a fixed increment inside the current
-                    bundle?.putString(player.playbackState.toString(), player.currentTimeline.toString());
+                    bundle?.putString(player.playbackState.toString(), count++.toString());
                     fireBaseModule.logEvent(player.playbackState.toString(), bundle)
                 }
                 Player.COMMAND_SEEK_BACK ->{
                     //Command to seek forward by a fixed increment inside the current
-                    bundle?.putString(player.playbackState.toString(), player.currentTimeline.toString());
+                    bundle?.putString(player.playbackState.toString(), count++.toString());
                     fireBaseModule.logEvent(player.playbackState.toString(), bundle)
                 }
                 else -> {
